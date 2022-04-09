eeb02e1113fc95d59ec35447ecdca488043eb119
keymergamez <keymergamez@gmail.com>
2022-04-09 16:57:44 -0400

    Avances del proyecto

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-import validator from './validator.js';
+import validator from './validator.js'
 
-console.log(validator);
+document.getElementById("myModal").style.display = "none";
+ 
+
+window.getCreditCardNumber = function getCreditCardNumber() {
+    const creditCardNumber = document.getElementById("card-number").value;
+
+    if (creditCardNumber === "") {
+        alert('Ingrese un número de tarjeta válida');
+        return
+    }
+
+    if (creditCardNumber.length < 13 || creditCardNumber.length > 16) {
+        alert('El número de tarjeta tiene que ser mayor a 13 y menor a 16 dígitos.');
+        return
+    }
+
+    const creditCardNumberMask = validator.maskify(creditCardNumber)
+
+    if (validator.isValid(creditCardNumber)) {
+        document.getElementById("myModal").style.display = "block";
+        document.getElementById("pmyModal").innerText = `${creditCardNumberMask} \n Tarjeta valida`
+       
+    } 
+    else {
+        document.getElementById("myModal").style.display = "block";
+        document.getElementById("pmyModal").innerText = `${creditCardNumberMask} \n Tarjeta invalida`
+    
+    }
+
+  }
+window.appformatexpirationdate = function appformatexpirationdate(value) { 
+  return value.replace(/^(\d\d)(\d)$/g,'$1/$2');
+}
+window.appformatcardNumber = function appformatcardNumber(value) {
+  return value.replace(/^[a-zA-Z]*$/g,'');                            
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,37 @@
 const validator = {
-  // ...
-};
+  isValid(creditCardNumber) {
+      if (creditCardNumber === null || (isNaN(creditCardNumber) === true)) {
+          return false
+      }
+
+      let sum = 0;
+      let cardNumberArray = creditCardNumber.split("");
+
+      for (let i = 0; i <= cardNumberArray.length - 1; i++) { 
+          cardNumberArray[i] = parseInt(cardNumberArray[i], 10);
+          if (i % 2 === 0) {
+              cardNumberArray[i] *= 2;
+              if (cardNumberArray[i] > 9) {
+                  cardNumberArray[i] = (cardNumberArray[i] % 10) + 1;
+              };
+          };
+          sum += cardNumberArray[i];
+
+          
+      }
+
+      if (sum % 10 === 0 && sum !== 0) {
+          return true
+      }
+
+      return false
+  },
+  maskify(creditCardNumber) {
+      return creditCardNumber.slice(0, -4).replace(/./g, "#") + creditCardNumber.slice(-4); // P RGEXP
+  }
+}
 
 export default validator;
+
+// 5491621005008394
+// ############8394
